f040cfa7ad1319fe61b77e79d9b77f10d71ebf48
Arnab S <arnabsengupta104@gmail.com>
2025-02-17 01:52:37 +0530

    more APIs

---
+++ client/app/api/createUser/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectToDatabase from "@/app/lib/mongo";
+import { User } from "@/app/models/schema";
+import { UserInterface } from "@/types";
+
+
+async function postHandler(request: NextRequest) {
+    await connectToDatabase();
+
+    const { alias, publicKey, role }: UserInterface = await request.json();
+
+    const user = await User.create({ alias, publicKey, role });
+
+    return NextResponse.json({ success: true, user }, { status: 200 });
+}
+
+export {
+    postHandler as POST
+};
