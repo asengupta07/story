1bc752df5b19f9778b1d7c215032a2c28e4a7810
Prayas <prayaspal04@gmail.com>
2025-02-17 03:05:23 +0530

    merge

--- client/app/providers/privyConfig.ts
+++ client/app/providers/privyConfig.ts
@@ -1,6 +1,5 @@
 import type { PrivyClientConfig } from '@privy-io/react-auth';
 
-// Replace this with your Privy config
 export const privyConfig: PrivyClientConfig = {
     embeddedWallets: {
         createOnLogin: 'users-without-wallets',
